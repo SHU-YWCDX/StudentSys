eb96f957dcc2bb5c7f57a13767911c36dd552d3e
ZraryZU <zurui@shu.edu.cn>
2023-02-14 14:14:40 +0800

    second commit

--- Students_System/settings.py
+++ Students_System/settings.py
@@ -31,12 +31,19 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+
+   # 'StudentSys'
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'StudentSys.apps.StudentsysConfig',
+    
+    'bootstrap4',
+
 ]
 
 MIDDLEWARE = [
