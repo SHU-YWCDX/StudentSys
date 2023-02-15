9a2a218fa30a21eec2f84b8c65b43c44e00e2ac6
ZraryZU <zurui@shu.edu.cn>
2023-02-15 22:07:14 +0800

    form

--- StudentSys/forms.py
+++ StudentSys/forms.py
@@ -37,3 +37,21 @@ class Crs_Form(forms.ModelForm):
                   'Crs_Credit':'学分',
                   'Crs_Period': '学时',
                   'Crs_Department':'院系',}
+
+class Tea_Form(forms.ModelForm):
+
+    class Meta:
+        model = models.TchInfo
+        fields = ('Tch_ID',
+                  'Tch_Name',
+                  'Tch_Department',
+                  'Tch_Position',
+                  'Tch_Username',
+                  'Tch_Password',)
+
+        labels = {'Tch_ID':'工号',
+                  'Tch_Name':'姓名',
+                  'Tch_Department':'院系',
+                  'Tch_Position':'职位',
+                  'Tch_Username':'用户名',
+                  'Tch_Password':'密码',}
