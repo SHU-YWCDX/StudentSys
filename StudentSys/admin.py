31fa2ca385d2060dd98a28809174ef67b9a348cf
ZraryZU <zurui@shu.edu.cn>
2023-02-15 19:10:47 +0800

    course前端

--- StudentSys/admin.py
+++ StudentSys/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from StudentSys import models
 # Register your models here.
+admin.site.register(models.CourseInfo)
+admin.site.register(models.TchInfo)
+admin.site.register(models.StuInfo)
